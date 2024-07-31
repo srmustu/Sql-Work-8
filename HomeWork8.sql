6f7ded2369d134eaa5cc58eb327fcddbf8c9a2f7
srmustu <127815561+srmustu@users.noreply.github.com>
2024-07-31 05:38:05 +0300

    Add files via upload

---
+++ HomeWork8.sql
@@ -0,0 +1,90 @@
+---CREATE TABLE employee(
+--	id INTEGER PRIMARY KEY,
+--	name VARCHAR(50) NOT NULL,
+--	birthday DATE,
+--	email VARCHAR(100) NOT NULL
+--);
+
+
+--insert into employee (id, name, birthday, email) values (1, 'Ozzie', '1972/02/17', 'oashe0@amazon.co.jp');
+--insert into employee (id, name, birthday, email) values (2, 'Gualterio', '1904/07/29', 'gclimpson1@smh.com.au');
+--insert into employee (id, name, birthday, email) values (3, 'Lowell', '2007/04/08', 'lburgan2@google.com.hk');
+--insert into employee (id, name, birthday, email) values (4, 'Kellia', '1993/08/17', 'kkendall3@who.int');
+--insert into employee (id, name, birthday, email) values (5, 'Justin', '1915/03/19', 'jscotson4@cam.ac.uk');
+--insert into employee (id, name, birthday, email) values (6, 'Pembroke', '1993/01/22', 'pmcgiven5@globo.com');
+--insert into employee (id, name, birthday, email) values (7, 'Dotti', null, 'dgauntlett6@webnode.com');
+--....
+--insert into employee (id, name, birthday, email) values (96, 'Miguela', '1961/04/22', 'mrousby2n@photobucket.com');
+--insert into employee (id, name, birthday, email) values (97, 'Alvin', '2016/02/08', 'aboler2o@sitemeter.com');
+--insert into employee (id, name, birthday, email) values (98, 'Ode', '1962/08/08', 'oduffill2p@mapquest.com');
+--insert into employee (id, name, birthday, email) values (99, 'Shaine', '1994/03/29', 'sworthy2q@biglobe.ne.jp');
+--insert into employee (id, name, birthday, email) values (100, 'Serena', '1913/08/24', 'seastes2r@mysql.com');
+
+
+--UPDATE employee
+--SET name = 'Mahmut',
+--	birthday = '1950.12.30',
+--	email = 'crazymahmut@gmail.com'
+--WHERE id = 3;
+
+--UPDATE employee
+--SET name = 'Ciguli',
+--	birthday = '1980.11.17',
+--	email = 'ciguuli@gmail.com'
+--WHERE id = 5;
+
+--UPDATE employee
+--SET name = 'Farah',
+--	birthday = '2000.09.17',
+--	email = 'fffarrahh@gmail.com'
+--WHERE id = 9;
+
+--UPDATE employee
+--SET name = 'Osman',
+--	birthday = '2002.03.05',
+--	email = 'usmanaga@gmail.com'
+--WHERE id = 11;
+
+--UPDATE employee
+--SET name = 'Vade',
+--	birthday = '1999.02.17',
+--	email = 'vadelivade@gmail.com'
+--WHERE id = 13;
+
+--UPDATE employee
+--SET name = 'Pelin',
+--	birthday = '1989.05.19',
+--	email = 'jale@gmail.com'
+--WHERE name LIKE 'B%'
+--RETURNING *;
+
+
+--DELETE FROM employee
+--WHERE name = 'JALE'
+--RETURNING *;
+
+
+--DELETE FROM employee
+--WHERE name LIKE 'C%'
+--RETURNING *;
+
+--DELETE FROM employee
+--WHERE name ILIKE '%b%'
+--RETURNING *;
+
+--DELETE FROM employee
+--WHERE id = '59'
+--RETURNING *;
+
+--DELETE FROM employee
+--WHERE name LIKE '%p%'
+--RETURNING *;
+
+--DELETE FROM employee
+--WHERE email LIKE '%k%'
+--RETURNING *;
+
+
+
+
+
